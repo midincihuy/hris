00a9b37650337627a3d12a5b70787cc780a8a5bf
Hamidin Hidayat <mailnidimah.hamidin@gmail.com>
2018-04-27 20:01:19 +0700

    add fillable Menu

--- app/Menu.php
+++ app/Menu.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Menu extends Model
 {
 
+  protected $fillable = [
+      'id', 'parent_id', 'text', 'label', 'can', 'url', 'icon',
+  ];
+
   public function submenu()
   {
     return $this->hasMany('App\Menu', 'parent_id')->select(['id','text','label','can','url','icon']);
